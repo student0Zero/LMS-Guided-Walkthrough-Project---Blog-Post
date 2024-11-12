7c0fdac5e4204205c13bc998c532bd76518d1e74
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-11-12 15:02:08 +0000

    Corrected typo in setting.py - import os

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import osimport dj_database_url
+import os 
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
