61629b478c6c0845f3667a5d206c9c7b51a319a7
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-11-13 17:15:02 +0000

    methods in our models

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 class Comment(model.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey( User, on_delete=models.CASCADE, related_name="commenter")
