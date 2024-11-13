46a55ac9459db3293b3a295ae7952db9d0cb60b4
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-11-13 13:58:57 +0000

    Deploy project

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-student0zer-lmsguidedwa-m1bgpo42onx.ws.codeinstitute-ide.net",
 ".herokuapp.com"
