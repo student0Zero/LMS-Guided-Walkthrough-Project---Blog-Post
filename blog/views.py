e8780b6ef8d56c1a787b940e1801a1996842b331
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-11-18 15:33:05 +0000

    corrected error on blog/view.py

--- blog/views.py
+++ blog/views.py
@@ -43,18 +43,21 @@ def post_detail(request, slug):
 
     comment_form = CommentForm()
 
-    return render(request, "blog/post_detail.html",
-    {
-        "post": post,
-        "comments": comments,
-        "comment_count": comment_count,
-        "comment_form": comment_form,
-    },
+    return render(
+        request,
+         "blog/post_detail.html",
+         {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
+        },
 )
 
 
 def comment_edit(request, slug, comment_id):
-    """view to edit comments
+    """
+    view to edit comments
     """
 
     if request.method == 'POST':
@@ -62,16 +65,16 @@ def comment_edit(request, slug, comment_id):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comment = get_object_or_404(Comment, pk=comment_id)
-        comment_form - CommentForm(data=request.POST, instance=comment)
+        comment_form = CommentForm(data=request.POST, instance=comment)
 
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.approved = False
             comment.save()
-            message.add_message(request, messages.SUCCESS, 'Comment Updated')
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated')
         else:
-            message.add_message(request, messages.ERROR, 'Error updating comment')
+            messages.add_message(request, messages.ERROR, 'Error updating comment')
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
