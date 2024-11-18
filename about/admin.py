6dd52fda86a482b2f19a83286bce381268e5baaa
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-11-18 22:07:59 +0000

    tidy up and added comments to classes

--- about/admin.py
+++ about/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
+from .models import About, CollaborateRequest
 
 # Register your models here.
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-
+    """
+    Adds rich-text editing of content in admin
+    """
     summernote_fields = ('content',)
 
 
@@ -18,5 +20,7 @@ class AboutAdmin(SummernoteModelAdmin):
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
-
+    """
+    Lists message and read fields for display in admin
+    """
     list_display = ('message', 'read',)
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,20 @@ from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
+    """
+    Returns all published posts in :model:`blog.Post`
+    and displays them in a page of six posts.
+    **Context**
+
+    ``queryset``
+        All published instances of :model:`blog.Post`
+    ``paginate_by``
+        Number of posts per page.
+
+    **Template:**
+
+    :template:`blog/index.html`
+    """
     queryset = Post.objects.filter(status=1)
     template_name = 'blog/index.html'
     paginate_by = 6
@@ -18,8 +32,14 @@ def post_detail(request, slug):
 
     **Context**
 
-    ''post''
-        An instance of :model:'blog.Post',
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comments``
+        All approved comments related to the post.
+    ``comment_count``
+        A count of approved comments related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
 
     **Template:**
 
@@ -57,9 +77,17 @@ def post_detail(request, slug):
 
 def comment_edit(request, slug, comment_id):
     """
-    view to edit comments
-    """
+    Display an individual comment for edit.
 
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
+    """
     if request.method == 'POST':
 
         queryset = Post.objects.filter(status=1)
@@ -81,7 +109,14 @@ def comment_edit(request, slug, comment_id):
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    Delete an individual comment.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
